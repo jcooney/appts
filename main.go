56926aa943abc7ce3b122af21e5a1471be828ef1
jcooney <jayfcooney@gmail.com>
2025-10-02 11:39:53 +0100

    feat(appts): inject real service into appointment creator

--- main.go
+++ main.go
@@ -10,10 +10,14 @@ import (
 	"time"
 
 	"github.com/jcooney/appts/api"
+	"github.com/jcooney/appts/domain"
 )
 
 func main() {
-	server := &http.Server{Addr: "0.0.0.0:3333", Handler: api.ChiHandler()}
+	// dependencies initialised here
+	service := domain.NewAppointmentCreatorService(nil, nil)
+
+	server := &http.Server{Addr: "0.0.0.0:3333", Handler: api.ChiHandler(service)}
 
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
